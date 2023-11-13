83c0929f59d5d5a6b04316aaf61e7e07c9c5204b
jrobles0717 <j_robles0717@yahoo.com>
2023-11-12 23:18:48 -0400

    Added sass configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/sass/main.scss";`, // Import your main.scss file here
-      },
-    },
-  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import "./sass/main.scss";`, // Import your main.scss file here
+  //     },
+  //   },
+  // },
 });
